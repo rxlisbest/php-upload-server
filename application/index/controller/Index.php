2f232bdef1dc6e0332c968d0742cf2692b79e2a9
Roy <rxlisbest@gmail.com>
2018-08-16 13:24:13 +0800

    增加上传功能

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,15 +1,24 @@
 <?php
 namespace app\index\controller;
 
+use Rxlisbest\SliceUpload\SliceUpload;
+use think\Request;
+
 class Index
 {
-    public function index()
+    public function index(Request $request)
     {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:) </h1><p> ThinkPHP V5.1<br/><span style="font-size:30px">12载初心不改（2006-2018） - 你值得信赖的PHP框架</span></p></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=64890268" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="eab4b9f840753f8e7"></think>';
-    }
-
-    public function hello($name = 'ThinkPHP5')
-    {
-        return 'hello,' . $name;
+        $post = $request->post();
+        $token = $request->post('token');
+        $token = explode(':', $token);
+        $param = json_decode(base64_decode($token[2]), true);
+        $dir = '/Library/WebServer/Documents/htdocs/upload_server/public/upload' . '/' . $param['scope'];
+        if (!file_exists($dir)) {
+            mkdir($dir, 0777, true);
+        }
+        $filename = $dir . '/' . $post['key'];
+        $slice_upload = new SliceUpload();
+        $result = $slice_upload->saveAs($filename);
+        var_dump($result);exit;
     }
 }
