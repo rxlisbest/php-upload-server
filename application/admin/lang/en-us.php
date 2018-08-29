cb04a816f239a00059fbdbf7f41ad669a9e720c3
Roy <rxlisbest@gmail.com>
2018-08-29 13:03:47 +0800

    增加语言包功能

---
+++ application/admin/lang/en-us.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruixinglong
+ * Date: 2018/8/29
+ * Time: 下午12:38
+ */
+return [
+    'login_index_title'  => 'Login',
+    'login_index_logo_title'  => 'RRS Administrator',
+    'login_index_input_user_name' => 'User Name',
+    'login_index_input_password' => 'Password',
+    'login_index_input_remember_me' => 'Remember Me',
+    'login_index_button_submit' => 'Submit',
+];
---
+++ application/admin/lang/zh-cn.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruixinglong
+ * Date: 2018/8/29
+ * Time: 下午12:38
+ */
+return [
+    'login_index_title'  => '登录',
+    'login_index_logo_title'  => 'RRS 后台管理系统',
+    'login_index_input_user_name' => '用户名',
+    'login_index_input_password' => '密码',
+    'login_index_input_remember_me' => '记住我',
+    'login_index_button_submit' => '提交',
+];
